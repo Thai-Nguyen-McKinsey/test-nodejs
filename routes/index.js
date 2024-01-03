97ee20c3cb6680c80926289a6d0939498c154781
Thai Q Nguyen <Thai_Q_Nguyen@mckinsey.com>
2024-01-03 13:40:18 +0700

    1st commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+// host: 'c-cosmo-postgres.soovqyprdlutqz.postgres.cosmos.azure.com port=5432 dbname=citus user=citus password=P@ssword123456 sslmode=require',
+const { Pool } = require('pg')
+const client = new Pool({
+  user: 'citus',
+  host: 'c-cosmo-postgres.soovqyprdlutqz.postgres.cosmos.azure.com',
+  database: 'citus',
+  password: 'P@ssword123456',
+  port: 5432,
+  ssl: true
+})
+
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    client.query('SELECT * FROM item;', (err, result) => {
+      console.log(result)
+      console.log('after res')
+      res.render('index', { title: 'Load new instance', items: result.rows });
+      // client.end();
+    })
+});
+
+
+router.get('/abc', (req, res) => {
+  res.json({ id: 213, description: "ifoas dasdhoi asdoas 'dasdasdh lasdsad", message: 'Hello, World!' });
+});
+
+module.exports = router;
